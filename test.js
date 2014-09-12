8246d95c19f489d9ab3439d280b0af1240553fad
tearsofphoenix <tearsofphoenix@icloud.com>
2014-09-12 09:43:14 +0800

    will merge.

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
 (function() {
-	 var tmr = new TMR('localhost:8080');
-	 console.log(tmr);
-	 tmr.set({a : 'info'});
-	 
+//	 var tmr = new TMR('localhost:8080');
+//	 console.log(tmr);
+//	 tmr.set({a : 'info'});
+
+    function sa(a) {
+        for (var b = [], c = 0, d = 0; d < a.length; d++) {
+            var e = a.charCodeAt(d);
+            55296 <= e && 56319 >= e && (e -= 55296, d++, fb_assert(d < a.length, "Surrogate pair missing trail surrogate."), e = 65536 + (e << 10) + (a.charCodeAt(d) - 56320));
+            128 > e ? b[c++] = e : (2048 > e ? b[c++] = e >> 6 | 192 : (65536 > e ? b[c++] = e >> 12 | 224 : (b[c++] = e >> 18 | 240, b[c++] = e >> 12 & 63 | 128), b[c++] = e >> 6 & 63 | 128), b[c++] = e & 63 | 128)
+        }
+        return b
+    }
+
+    var str = sa('你好');
+    console.log(str);
 })();
