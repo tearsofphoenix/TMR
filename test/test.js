5d9a76165415b3c7ca3af3c5d236ab685be2f795
Lei <tearsofphoenix@icloud.com>
2014-09-19 21:58:15 +0800

    web socket connect error.

--- test/test.js
+++ test/test.js
@@ -1,4 +1,10 @@
 (function() {
     var timestamp = (new Date()).getTime();
     console.log(tmr_encode_timestamp(timestamp));
+
+    var tmr = new TMR('http://127.0.0.1/data');
+    console.log(tmr);
+    tmr.set({a : 'hello world!'}, function(error) {
+        console.log(error);
+    })
 })();
--- test/websocket_server.js
+++ test/websocket_server.js
@@ -1,118 +1,125 @@
 #!/usr/bin/env node
-var WebSocketServer = require('websocket').server;
-var http = require('http');
 
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
-});
+(function() {
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
-
-function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
-}
 
 //@return string
-var UUID = function() {
-		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-						var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-						return v.toString(16);
-    				});
-	};
+    function UUID() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+    };
 
-var Db = require('mongodb').Db, Connection = require('mongodb').Connection,
-  Server = require('mongodb').Server;
 
-var client = new Db('test', new Server("127.0.0.1", 27017, {}), {safe: false}); 
-client.open(function(err, p_client) {
-/*   client.collection('test_insert', test); */
-});
+    var TMRCollectionPool = {};
 
-var TMRCollectionPool = {};
+    function TMRServer() {
 
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
+        var WebSocketServer = require('websocket').server;
+        var http = require('http');
+
+        var server = http.createServer(function(request, response) {
+            console.log((new Date()) + ' Received request for ' + request.url);
+            response.writeHead(404);
+            response.end();
+        });
+        server.listen(8080, function() {
+            console.log((new Date()) + ' Server is listening on port 8080');
+        });
+
+        this._socketServer =  new WebSocketServer({
+            httpServer: server,
+            autoAcceptConnections: false
+        });
+
+        //create db connection
+        var Db = require('mongodb').Db, Connection = require('mongodb').Connection,
+            Server = require('mongodb').Server;
+
+        this._client = new Db('test', new Server("127.0.0.1", 27017, {}), {safe: false});
+        this._client.open(function(err, client) {
+            /*   client.collection('test_insert', test); */
+        });
+
+        this._socketServer.on('request', function(request) {
+            if (!this.originIsAllowed(request.origin)) {
+                // Make sure we only accept requests from an allowed origin
+                request.reject();
+                console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+                return;
+            }
+
+            var connection = request.accept('tmr-protocol', request.origin);
+
+            connection._data = [];
+            connection._count = 0;
+
+            console.log((new Date()) + ' Connection accepted.');
+
+            var dummy = this;
+            connection.on('message', function(message) {
+
+                var data = message.utf8Data;
+                if (connection._count == 0) {
+                    connection._count = parseInt(data);
+                    if (0 == connection._count){
+                        throw Error('Invalid data length');
+                    }
+                }else{
+                    connection._data.push(data);
+                    if (connection._data.length == connection._count) {
+                        //have received all data
+                        //
+                        var msg = connection._data.join('');
+                        var info = JSON.parse(msg);
+                        console.log('recived message: ' + msg);
+                        if (info) {
+                            switch(info.action) {
+                                case 'insert': {
+                                    connection.insert(info.data, function(error, docs) {
+                                        if (error) {
+                                            console.log(error);
+                                            dummy._socketServer.sendUTF(error.toString());
+                                        }
+                                    });
+                                    break;
+                                }
+                                case 'collection': {
+                                    client.collection(info.name, function(error, collection) {
+                                        if(error) {
+                                            dummy._socketServer.sendUTF(JSON.stringify({status: -1, error: error.toString(), msg: msg, msgID: info.msgID}) );
+                                        }else{
+                                            var uuid = UUID();
+                                            TMRCollectionPool[uuid] = collection;
+                                            dummy._socketServer.sendUTF(JSON.stringify({status: 0, uuid: uuid, msgID: info.msgID }));
+                                        }
+                                    });
+                                    break;
+                                }
+                                default: {
+                                    break;
+                                }
+                            }
+                        }
+                        this.sendUTF(data);
+
+                        connection._count = 0;
+                        connection._data = [];
+                    }
+                }
+            });
+
+            connection.on('close', function(reasonCode, description) {
+                console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+            });
+        });
     }
 
-    var connection = request.accept('tmr-protocol', request.origin);
-    
-    connection._data = [];
-    connection._count = 0;
-    
-    console.log((new Date()) + ' Connection accepted.');
-    
-    connection.on('message', function(message) {
-    
-    	var data = message.utf8Data;
-    	if (this._count == 0) {
-	    	this._count = parseInt(data);
-	    	if (0 == this._count){
-		    	throw Error('Invalid data length');
-	    	}
-    	}else{
-	    	this._data.push(data);
-	    	if (this._data.length == this._count) {
-		    	//have received all data
-		    	//
-		    	var msg = this._data.join('');
-		    	var info = JSON.parse(msg);
-				console.log('recived message: ' + msg);
-				
-		    	if (info) {
-			    	switch(info.action) {
-				    	case 'insert': {
-				    		connection.insert(info.data, function(error, docs) {
-					    		if (error) {
-						    		console.log(error);
-						    		this.sendUTF(error.toString());
-					    		}
-				    		});
-					    	break;
-				    	}
-				    	case 'collection': {
-							client.collection(info.name, function(error, collection) {
-								if(error) {
-									this.sendUTF(JSON.stringify({status: -1, error: error.toString(), msg: msg, msgID: info.msgID}) );
-								}else{
-									var uuid = UUID();
-									TMRCollectionPool[uuid] = collection;
-									this.sendUTF(JSON.stringify({status: 0, uuid: uuid, msgID: info.msgID }));
-								}
-							});				    	
-					    	break;
-				    	}
-				    	default: {
-					    	break;
-				    	}
-			    	}
-		    	}
-				this.sendUTF(data);
-				
-				this._count = 0;
-				this._data = [];
-	    	}
-    	}
-    });
-    
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
-});
+    TMRServer.prototype.originIsAllowed = function(origin) {
+        console.log(request.origin);
+        return true;
+    };
+
+    return new TMRServer();
+})();
